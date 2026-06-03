7a94427d05bf2e04f0b0cd6bad2a1ca2c01192ac
bruhboycarti <popkovs194@gmail.com>
2026-06-03 17:48:37 +0300

    Add files via upload

---
+++ InvalidRecIntegralException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class InvalidRecIntegralException extends Exception {
+
+    public InvalidRecIntegralException(String message) {
+        super(message);
+    }
+
+    public InvalidRecIntegralException(String message, Throwable cause) {
+        super(message, cause);
+    }
+} 
